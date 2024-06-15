ad1a9f1964146d79ed01c8fac62c7e8f101c33df
Tinashe Matanga <tinashe.valarie.m@gmail.com>
2024-06-15 17:10:03 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import Flask, render_template
+#imported module and creating an app using Flask
+app = Flask('index.html')
+
+#defining the basic route "/" and its corresponding request handler
+@app.route("/")
+def main():
+    return "Welcome!"
+#checking if the executed file is the main program and running the app
+if __name__ == "__main__":
+    app.run()
+
+
